c72de2303d0251378d7505e371c475bc46103f82
Ameer <ameerapex496@gmail.com>
2023-11-04 00:31:35 +0100

    finished functions for the project

--- src/script.js
+++ src/script.js
@@ -1,1 +1,25 @@
-console.log("start of project");
+const videoObj = document.getElementById("video");
+const startbtn = document.getElementsByClassName("startbtn")[0];
+
+async function pictureFunc() {
+  try {
+    const mediaStream = await navigator.mediaDevices.getDisplayMedia();
+    videoObj.srcObject = mediaStream;
+    videoObj.onloadedmetadata = () => {
+      videoObj.play();
+    };
+  } catch (error) {
+    console.log("you got an error:", error);
+  }
+}
+
+startbtn.addEventListener("click", async () => {
+  // Corrected "click" event and "addEventListener" function
+  startbtn.disabled = true;
+
+  await videoObj.requestPictureInPicture(); // Corrected "videoOjb" to "videoObj"
+
+  startbtn.disabled = false;
+});
+
+pictureFunc();
